3bfa7af1b752eff9b7ba1f3fb1a611be3451d5e4
dan-suen <danielsuen200791357@gmail.com>
2025-02-07 01:59:15 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+const stockfish = spawn('./stockfish-windows-x86-64-avx2.exe');
+
+stockfish.stdin.write('uci\n');
+
+app.post('/uci', (req, res) => {
+  const { command } = req.body;
+  stockfish.stdin.write(command + '\n');
+  let output = '';
+
+  stockfish.stdout.on('data', (data) => {
+    output += data.toString();
+  });
+
+  stockfish.stdout.on('end', () => {
+    res.send({ response: output });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
